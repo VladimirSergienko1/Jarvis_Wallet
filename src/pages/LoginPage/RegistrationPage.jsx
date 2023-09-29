0d8f2a3a25874f7cafa07f8e554179e452942c37
vladimir <vladimir.945g@gmail.com>
2023-09-29 16:58:26 +0600

    added formik library for validation

--- src/pages/LoginPage/RegistrationPage.jsx
+++ src/pages/LoginPage/RegistrationPage.jsx
@@ -8,6 +8,26 @@ import CustomSelect from "./CustomSelect.jsx";
 import {useState} from "react";
 import {Link} from "react-router-dom";
 
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+const validationSchema = Yup.object({
+    name: Yup.string()
+        .min(3, 'Name should be 3')
+        .max(8, 'Name should be 8')
+        .required('Name is required'),
+    email: Yup.string()
+        .email('Invalid email format')
+        .required('Email is required')
+        .matches(
+            /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/,
+            "Invalid email format"
+        ),
+    phone: Yup.string(),
+    password: Yup.string(),
+    rePassword: Yup.string(),
+
+});
 const RegistrationPage = () =>{
     const options = [
         { value: 'eng', label: 'English' },
@@ -15,97 +35,63 @@ const RegistrationPage = () =>{
         { value: 'kz', label: 'Kazakh' },
     ];
     const [selectedOption, setSelectedOption] = useState({ value: 'eng', label: 'English' });
-    const [name, setName] = useState('');
-    const [nameDirty, setNameDirty] = useState(false);
-    const [nameError, setNameError] = useState('Name field is required');
-    const [nameIcon, setNameIcon] = useState();
-    const [email, setEmail] = useState('');
-    const [emailDirty, setEmailDirty] = useState(false);
-    const [emailError, setEmailError] = useState('Email field is required');
     const [emailIcon, setEmailIcon] = useState();
-    const [password, setPassword] = useState('');
-    const [passwordDirty, setPasswordDirty] = useState(false);
-    const [passwordError, setPasswordError] = useState('Password field is required');
-    const [passwordIcon, setPasswordIcon] = useState();
-    const [rePassword, setRePassword] = useState('');
-    const [rePasswordDirty, setRePasswordDirty] = useState(false);
-    const [rePasswordError, setRePasswordError] = useState('Repeat password field is required');
-    const [rePasswordIcon, setRePasswordIcon] = useState();
 
-
-    const nameHandler = (e) =>{
-        setName(e.target.value);
-        if(e.target.value.length < 3 || e.target.value.length > 8){
-            setNameError('Name should be < 3 > 8');
-            setNameIcon(errorIcon)
-            setNameDirty(true)
-        }
-        else {
-            setNameError('');
-            setNameIcon(correctIcon)
-            setNameDirty(false)
-        }
-    }
-    const blurHandler = (e)=>{
-        switch (e.target.name) {
-            case 'name':
-                setNameDirty(true);
-
-                break;
-            default:
-                break;
-        }
-    }
-
-
-    const onSubmit = (e) =>{
-        e.preventDefault();
-        if (name.length === 0){
-            setNameError('Name field is required');
-            setNameIcon(errorIcon);
-            setNameDirty(true);
-        }
-        if (email.length === 0){
-            setEmailError('Email field is required');
-            setEmailIcon(errorIcon);
-            setEmailDirty(true);
-        }
-
-        const data = new FormData(e.target);
-        console.log(Object.fromEntries(data.entries()))
-
-    }
-
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            phone: '',
+            password: '',
+            rePassword: ''
+            // Другие поля
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            console.log(values);
+            // Обработка отправки формы
+        },
+    });
 
     return(
         <div className={styles.registration_page}>
             <div className={styles.registration_page__container}>
                 <img src={info} alt="Info"/>
                 <div className={styles.reg_block}>
-                    <form className={styles.reg_form} onSubmit={onSubmit}>
+                    <form className={styles.reg_form} onSubmit={formik.handleSubmit}>
                         <p className={styles.reg_title}>New user</p>
                         <label htmlFor={'name_input'} className={styles.reg_label_required}>Name</label>
                         <div className={styles.input_container}>
                             <input
-                                name={'name'}
-                                onBlur={blurHandler}
-                                id={'name_input'}
+                                id="name_input"
+                                name="name"
+                                type="text"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.name}
                                 className={styles.reg_input}
-                                value={name}
-                                onChange={nameHandler}
                             />
-                            {(nameError && nameDirty) && <p className={styles.error_text}>{nameError}</p>}
-                            <img className={styles.error_img} src={nameIcon}/>
+                            {formik.touched.name && formik.errors.name && (
+                                <p className={styles.error_text}>{formik.errors.name}</p>
+                            )}
+                            <img className={styles.error_img} src={emailIcon}/>
 
                         </div>
                         <label htmlFor={'email_input'} className={styles.reg_label_required}>Email</label>
                         <div className={styles.input_container}>
                             <input
-                                id={'email_input'}
+                                id="email_input"
+                                name="email"
+                                type="email"
+                                onChange={formik.handleChange}
+                                onBlur={formik.handleBlur}
+                                value={formik.values.email}
                                 className={styles.reg_input}
-                                name={'Email'}
+
                             />
-                            {(emailError && emailDirty) && <p className={styles.error_text}>{emailError}</p>}
+                            {formik.touched.email && formik.errors.email && (
+                                <p className={styles.error_text}>{formik.errors.email}</p>
+                            )}
                             <img className={styles.error_img} src={emailIcon}/>
 
                         </div>
